a73fe7bf577d9a2dc051010cd74e51946392d474
nicholas.harrington1 <z17s466@csci331vm.cs.montana.edu>
2024-11-03 17:41:13 -0700

    Initial commit with Counter component and main page

---
+++ src/pages/index.jsx
@@ -0,0 +1,10 @@
+import Counter from '../components/Counter';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Nick Harrington's Counter App</h1>
+      <Counter />
+    </div>
+  )
+}
